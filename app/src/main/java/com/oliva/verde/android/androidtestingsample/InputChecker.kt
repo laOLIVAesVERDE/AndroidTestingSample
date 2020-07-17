1e637d1461062c98f84e08be6b16363465c69c29
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2020-07-17 16:39:26 +0900

    網羅性を考慮したテストケースの追加

--- app/src/main/java/com/oliva/verde/android/androidtestingsample/InputChecker.kt
+++ app/src/main/java/com/oliva/verde/android/androidtestingsample/InputChecker.kt
@@ -2,7 +2,7 @@ package com.oliva.verde.android.androidtestingsample
 
 class InputChecker {
     fun isValid(text : String?) : Boolean {
-        if(text == null) return false
+        if(text == null) throw IllegalArgumentException("Cannot be null")
         return text.length >= 3 && text.matches(Regex("[a-zA-Z0-9]+"))
     }
 }
--- app/src/test/java/com/oliva/verde/android/androidtestingsample/InputCheckerTest.kt
+++ app/src/test/java/com/oliva/verde/android/androidtestingsample/InputCheckerTest.kt
@@ -7,6 +7,7 @@ import org.junit.Test
 
 import org.junit.Assert.*
 import org.hamcrest.CoreMatchers.`is`
+import org.junit.Ignore
 
 
 class InputCheckerTest {
@@ -34,8 +35,38 @@ class InputCheckerTest {
     }
 
     @Test
+    fun isValis_givenAlphabetic_returnsTrue() {
+        val actual = target.isValid("abc")
+        assertThat(actual, `is`(true))
+    }
+
+    @Test
+    fun isValis_givenNumeric_returnsTrue() {
+        val actual = target.isValid("123")
+        assertThat(actual, `is`(true))
+    }
+
+    @Test
     fun isValid_givenAlphaNumeric_returnsTrue() {
         val actual = target.isValid("abc123")
         assertThat(actual, `is`(true))
+    }
+
+    @Test
+    fun isValid_givenInvalidCharacter_returnsFalse() {
+        val actual = target.isValid("abc@123")
+        assertThat(actual, `is`(false))
+    }
+
+    // @Test(expected = 例外クラス) : テスト対象メソッドが意図通り例外を発生させたかを検証
+    @Test(expected = IllegalArgumentException::class)
+    fun isValid_givenNull_throwsIllegalArgumentException() {
+        target.isValid(null)
+    }
+
+    @Ignore("テスト対象が仮実装なので一時的にスキップ")
+    @Test
+    fun temporarilySkipThisTest() {
+        // 略
     }
  }
