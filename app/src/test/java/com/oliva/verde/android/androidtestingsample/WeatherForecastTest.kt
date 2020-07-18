13ba73bf56b765f11e9964c4d48fa53b7da17ac2
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2020-07-19 00:01:23 +0900

    Mockitoを用いたスパイによるテスト

--- app/src/test/java/com/oliva/verde/android/androidtestingsample/WeatherForecastTest.kt
+++ app/src/test/java/com/oliva/verde/android/androidtestingsample/WeatherForecastTest.kt
@@ -6,28 +6,43 @@ import org.junit.Test
 
 import org.junit.Assert.*
 import org.junit.Before
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import org.mockito.Spy
 
 class WeatherForecastTest {
+    @Mock
+    lateinit var satellite: Satellite
+    @Mock
     lateinit var recorder: WeatherRecorder
+    @Spy
+    val formatter: WeatherFormatter = WeatherFormatter()
+
     lateinit var weatherForecast: WeatherForecast
 
     @Before
     fun setUp() {
-        recorder = mock(name = "MockRecorder")
-
+        MockitoAnnotations.initMocks(this)
 
         val satellite = Satellite()
-        val formatter = WeatherFormatter()
+        val recorder = WeatherRecorder()
         weatherForecast = WeatherForecast(satellite, recorder, formatter)
     }
 
+    @Test
+    fun recordCurrentWeather_assertFormatterCalled() {
+        weatherForecast.recordCurrentWeather(37.580006, -122.345106)
+        verify(formatter, times(1)).format(any())
+    }
+
+    /** モックを用いたテスト
     @Test
     fun recordCurrentWeather_assertRecorderCalled() {
         weatherForecast.recordCurrentWeather(37.580006, -122.345106)
         verify(recorder, times(1)).record(any())
     }
 
-    /** スタブを用いたテスト
+    // スタブを用いたテスト
     @Test
     fun shouldBringUmbrella_givenInJapan_returnsFalse() {
         val actual = weatherForecast.shouldBringUmbrella(35.669784, 139.817728)
